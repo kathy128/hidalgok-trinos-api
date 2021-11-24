3529c2128ec74bc0cb831b880db990ef8c6b9ac9
kathy128 <hidalgok@uninorte.edu.co>
2021-11-24 01:14:02 -0500

    Add: migrations file

---
+++ src/database/migrations/20211124042156-create-tweet.js
@@ -0,0 +1,31 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Tweets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      text: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      likeCounter: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Tweets');
+  },
+};
--- src/database/models/user.js
+++ src/database/models/user.js
@@ -11,19 +11,18 @@ const { ROLES } = require('../../config/constants');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
+    
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
-    }
 
     comparePassword(plainTextPassword) {
       return bcrypt.compare(plainTextPassword, this.password);
-    }
+    };
   }
+  
   User.init({
     username: {
       type: DataTypes.STRING,
